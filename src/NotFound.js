940de5817b5f30346117612f6960ade8236d99bc
IjaasAhamed <ijaasahamed143@gmail.com>
2025-10-25 23:24:52 +0300

    Fixed Bugs in TouchCursor

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,19 +1,26 @@
 import './NotFound.css';
 import errorImg from './img/astronaut.png';
+import TargetCursor from './TargetCursor.js';
 
 const NotFound = () => {
   return (
-    <div className="not-found-container">
-      <div className="not-found-content">
-        <img src={errorImg} className='astronaut-image' alt="Lost Astronaut" />
-        <h1>Oops! 404 Not Found</h1>
-        <p>Sorry, the page you are looking for does not exist.</p>
-        <p>The project is currently offline, and its live version is temporarily unavailable.</p>
-        <a href="/">
-          <button className="btntrans mt-4">← Back to Portfolio</button>
-        </a>
+    <>
+      <TargetCursor
+        spinDuration={0}
+        hideDefaultCursor={true}
+      />
+      <div className="not-found-container">
+        <div className="not-found-content">
+          <img src={errorImg} className='astronaut-image' alt="Lost Astronaut" />
+          <h1>Oops! 404 Not Found</h1>
+          <p>Sorry, the page you are looking for does not exist.</p>
+          <p>The project is currently offline, and its live version is temporarily unavailable.</p>
+          <a href="/">
+            <button className="btntrans mt-4 cursor-target">← Back to Portfolio</button>
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
